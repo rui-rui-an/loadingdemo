abbe6278b3cef97f265aba004d6122987254b546
rui-rui-an <1405613785@qq.com>
2023-04-13 23:34:22 +0800

    bug修改

--- src/api/test.js
+++ src/api/test.js
@@ -17,18 +17,18 @@ function test2 (data,loadingObj) {
   })
 }
 // 不带loading的get请求，正常封装api
-function test () {
-  return _fetch({
-    url: '/getlist/test',
-    method: 'get'
-  })
-}
+// function test () {
+//   return _fetch({
+//     url: '/getlist/test',
+//     method: 'get'
+//   })
+// }
 // 不带loading的post请求，正常封装api
-function test2 (data) {
-  return _fetch({
-    url: '/getlist/test2',
-    method: 'post',
-    data
-  })
-}
+// function test2 (data) {
+//   return _fetch({
+//     url: '/getlist/test2',
+//     method: 'post',
+//     data
+//   })
+// }
 export { test,test2 }
